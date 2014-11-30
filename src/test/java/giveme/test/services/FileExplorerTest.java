7c3e1f6ec1e7547d0e141d708be649b7a5bc76d6
ogdabou <ogdabou@gmail.com>
2014-11-30 22:32:14 +0100

    Fixed test error on FileExplorerTest

--- src/test/java/giveme/test/services/FileExplorerTest.java
+++ src/test/java/giveme/test/services/FileExplorerTest.java
@@ -3,16 +3,11 @@ package giveme.test.services;
 import giveme.services.FileExplorer;
 
 import org.apache.log4j.Logger;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 public class FileExplorerTest
 {
 	public static Logger	LOGGER	= Logger.getLogger(FileExplorerTest.class.getName());
 
-	@Test
 	public void listTest()
 	{
 		LOGGER.info("TESTING FOLDER LIST");
