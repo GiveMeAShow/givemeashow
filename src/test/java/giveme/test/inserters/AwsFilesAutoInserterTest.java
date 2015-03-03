eb432a7a4b326bd38f087b975a8ae5c3fe2b6c36
ogdabou <ogdabou@gmail.com>
2015-03-03 15:22:23 +0100

    #68 forgot to commit test file

---
+++ src/test/java/giveme/test/inserters/AwsFilesAutoInserterTest.java
@@ -0,0 +1,65 @@
+package giveme.test.inserters;
+
+import giveme.inserters.AwsFilesAutoInserter;
+import giveme.inserters.Inserter;
+import giveme.shared.GiveMeProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
+public class AwsFilesAutoInserterTest {
+
+	private static final Logger	LOGGER	= Logger.getLogger(AwsFilesAutoInserterTest.class
+												.getName());
+
+	AwsFilesAutoInserter	awsFilesAutoInserter;
+
+	@Before
+	public void init() throws Exception {
+		awsFilesAutoInserter = new AwsFilesAutoInserter();
+		AmazonS3Client amazonS3Mock = Mockito.mock(AmazonS3Client.class);
+		
+		ObjectListing returnListMock = Mockito.mock(ObjectListing.class);
+		List<S3ObjectSummary> objctSummaries = new ArrayList<S3ObjectSummary>();
+		S3ObjectSummary sum1 = new S3ObjectSummary();
+		sum1.setKey("/American_dad");
+		S3ObjectSummary sum2 = new S3ObjectSummary();
+		sum2.setKey("/American_dad/Season_1/");
+		S3ObjectSummary sum3 = new S3ObjectSummary();
+		sum3.setKey("/American_dad/Season_1/fr");
+		S3ObjectSummary sum4 = new S3ObjectSummary();
+		sum4.setKey("/American_dad/Season_1/fr/video10.webm");
+		objctSummaries.add(sum1);
+		objctSummaries.add(sum2);
+		objctSummaries.add(sum3);
+		objctSummaries.add(sum4);
+		Mockito.doReturn(objctSummaries).when(returnListMock)
+				.getObjectSummaries();
+	
+		Mockito.doReturn(returnListMock).when(amazonS3Mock)
+				.listObjects(Mockito.anyString());
+	
+		awsFilesAutoInserter.setS3(amazonS3Mock);
+		Inserter inserterMock = Mockito.mock(Inserter.class);
+		awsFilesAutoInserter.setInserter(inserterMock);
+	}
+
+	@Test
+	public void visitAllTest() {
+		GiveMeProperties givemeAShowProperties = new GiveMeProperties();
+		givemeAShowProperties.setAWKEY("key");
+		givemeAShowProperties.setAWSECRET("secret");
+		awsFilesAutoInserter.setGiveMeAShowProperties(givemeAShowProperties);
+		awsFilesAutoInserter.visitAll();
+		LOGGER.info("visit");
+	}
+}
